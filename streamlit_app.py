2d3e1960463e4f23cb01658fee31006916e57c7c
nikhitha3122 <105553543+nikhitha3122@users.noreply.github.com>
2022-12-10 12:32:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,38 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
-import streamlit as st
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+def read_file(filename):
+  img=cv2.imread(filename)
+  img =cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+  return img
+filename="IMG11.jpeg"
+img=read_file(filename)
+image=np.copy(img)
+#create edge mask
+def edge_mask(img, line_size, blur_value):
+    gray=cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+    gray_blur= cv2.medianBlur(gray, blur_value)
+    
+    edges= cv2.adaptiveThreshold(gray_blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, line_size, blur_value)
+    return edges
+line_size, blur_value=7, 7
+edges= edge_mask(img, line_size, blur_value)
+def color_quantization(img, k):
+    data=np.float32(img).reshape((-1,3))
+    criteria=(cv2.TERM_CRITERIA_EPS+ cv2.TERM_CRITERIA_MAX_ITER, 20, 0.001)
+    ret , label, center = cv2.kmeans(data, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+    center= np.uint8(center)
+    result=center[label.flatten()]
+    result=result.reshape(img.shape)
+    
+    return result
+img=color_quantization(img, k=9)
+#reduce the noise
+blurred= cv2.bilateralFilter(img, d=4, sigmaColor= 200, sigmaSpace=200)
+def cartoon():
+    c=cv2.bitwise_and(blurred,blurred,mask=edges)
+    plt.imshow(c)
+    plt.axis('off')
+    plt.show()
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
-
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
-
-    Point = namedtuple('Point', 'x y')
-    data = []
-
-    points_per_turn = total_points / num_turns
-
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
-
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+cartoon()
